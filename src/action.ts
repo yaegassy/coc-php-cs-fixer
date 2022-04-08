9dcabdf95b63d26e62c85ef413a5a594c1e393f3
yaegassy <yaegassy@users.noreply.github.com>
2022-04-08 19:02:55 +0900

    refactor(action): disable except for php

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,14 @@
-import { TextDocument, CodeAction, CodeActionContext, CodeActionProvider, Range } from 'coc.nvim';
+import { CodeAction, CodeActionContext, CodeActionProvider, Command, Range, TextDocument } from 'coc.nvim';
 
 export class FixerCodeActionProvider implements CodeActionProvider {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public async provideCodeActions(document: TextDocument, range: Range, context: CodeActionContext) {
+    if (document.languageId !== 'php') return;
+
     const codeActions: CodeAction[] = [];
 
     const title = `Run: php-cs-fixer.fix`;
-    const command = {
+    const command: Command = {
       title: '',
       command: 'php-cs-fixer.fix',
     };
