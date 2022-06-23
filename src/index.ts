d5fde49c3a44e0d0a2cfd15052fb4fb912db50ae
yaegassy <yaegassy@users.noreply.github.com>
2022-06-23 15:11:24 +0900

    refactor: move to providers feature module

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,16 @@
-import { commands, Disposable, DocumentSelector, ExtensionContext, languages, window, workspace } from 'coc.nvim';
+import { commands, ExtensionContext, window, workspace } from 'coc.nvim';
 import fs from 'fs';
 import path from 'path';
 
 import * as fixCodeActionFeature from './actions/fix';
 import * as downloadCommandFeature from './commands/download';
 import * as fixCommandFeature from './commands/fix';
-import FixerFormattingEditProvider from './format';
-
-let formatterHandler: undefined | Disposable;
-
-function disposeHandlers(): void {
-  if (formatterHandler) {
-    formatterHandler.dispose();
-  }
-  formatterHandler = undefined;
-}
+import * as documentFormatProviderFeature from './providers/documentFormat';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const extensionConfig = workspace.getConfiguration('php-cs-fixer');
-  const isEnable = extensionConfig.get<boolean>('enable', true);
-  if (!isEnable) return;
-
-  const isEnableFormatProvider = extensionConfig.get<boolean>('enableFormatProvider', false);
+  if (!workspace.getConfiguration('php-cs-fixer').get<boolean>('enable', true)) return;
 
   const outputChannel = window.createOutputChannel('php-cs-fixer');
-
   const extensionStoragePath = context.storagePath;
   if (!fs.existsSync(extensionStoragePath)) {
     fs.mkdirSync(extensionStoragePath);
@@ -34,7 +20,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
   fixCommandFeature.activate(context, outputChannel);
   downloadCommandFeature.activate(context);
 
-  let toolPath = extensionConfig.get('toolPath', '');
+  let toolPath = workspace.getConfiguration('php-cs-fixer').get('toolPath', '');
   if (!toolPath) {
     if (fs.existsSync(path.join('vendor', 'bin', 'php-cs-fixer'))) {
       toolPath = path.join('vendor', 'bin', 'php-cs-fixer');
@@ -46,19 +32,8 @@ export async function activate(context: ExtensionContext): Promise<void> {
     commands.executeCommand('php-cs-fixer.download');
   }
 
-  const editProvider = new FixerFormattingEditProvider(context, outputChannel);
-
-  const priority = 1;
-  const languageSelector: DocumentSelector = [{ language: 'php', scheme: 'file' }];
-
-  function registerFormatter(): void {
-    disposeHandlers();
-
-    if (isEnableFormatProvider) {
-      formatterHandler = languages.registerDocumentFormatProvider(languageSelector, editProvider, priority);
-    }
-  }
-  registerFormatter();
+  // register DocumentFormatProvider
+  documentFormatProviderFeature.activate(context, outputChannel);
 
   // register code action feature
   fixCodeActionFeature.activate(context);
---
+++ src/providers/documentFormat.ts
@@ -0,0 +1,17 @@
+import { DocumentSelector, ExtensionContext, languages, OutputChannel, workspace } from 'coc.nvim';
+import FixerFormattingEditProvider from '../format';
+
+export function activate(context: ExtensionContext, outputChannel: OutputChannel) {
+  if (workspace.getConfiguration('php-cs-fixer').get<boolean>('enableFormatProvider', false)) {
+    const languageSelector: DocumentSelector = [{ language: 'php', scheme: 'file' }];
+    const priority = 1;
+
+    context.subscriptions.push(
+      languages.registerDocumentFormatProvider(
+        languageSelector,
+        new FixerFormattingEditProvider(context, outputChannel),
+        priority
+      )
+    );
+  }
+}
